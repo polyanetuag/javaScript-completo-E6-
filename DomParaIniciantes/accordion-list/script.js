5126a7a0fc16cb4b5a484d0a122ee120b5f616ce
Polyane Tuag <pollyportugues@hotmail.com>
2020-10-26 18:56:40 -0300

    Aula_ Accordion List

---
+++ DomParaIniciantes/accordion-list/script.js
@@ -0,0 +1,50 @@
+// navegação entre as imagens e a descrição dos animais
+
+// 1- selecionando os elementos
+function initTabNav() {
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+tabContent[0].classList.add('ativo')
+
+//2-função para alternar as classes que estão em ativo
+
+  if (tabMenu.length && tabContent.length) {        // 1.1 - verificação dos elementos
+    function activeTab(index) {
+      tabContent.forEach((section) => {              // para remover a classe ativo, foi usado o loop foreach
+        section.classList.remove('ativo')
+      })
+      tabContent[index].classList.add('ativo');         // adiciona a classe ativo
+    }
+    // 3- adicionando o evento
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index)
+      });
+    });
+  }
+}
+initTabNav();
+
+
+// ================= ACORDION LIST ================ outro tipo de navegação
+function initAccordion(){
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+
+  if(accordionList.length) {                                  // verificar se existe
+
+  accordionList[0].classList.add(activeClass)                     //começar o primeiro item aberto
+  accordionList[0].nextElementSibling.classList.add(activeClass)
+
+  function activeAccordion() {
+    this.classList.toggle(activeClass)
+    this.nextElementSibling.classList.toggle(activeClass);         // o this faz referencia ao item que está sendo selecionado
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+  });
+}
+}
+initAccordion();
