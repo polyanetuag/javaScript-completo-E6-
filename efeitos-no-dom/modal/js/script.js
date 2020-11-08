d0a5d208dbf271d0f5f4b344e7df98f8b9fef0d0
Polyane Tuag <pollyportugues@hotmail.com>
2020-11-08 15:11:26 -0300

    Aula_Modal 2

--- efeitos-no-dom/modal/js/script.js
+++ efeitos-no-dom/modal/js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
