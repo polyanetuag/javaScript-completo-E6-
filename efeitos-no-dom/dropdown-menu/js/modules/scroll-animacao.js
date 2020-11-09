0a750a2db0d82636e6d5c5a37391a69bf66fa61b
Polyane Tuag <pollyportugues@hotmail.com>
2020-11-09 19:36:30 -0300

    estrutura_projeto

---
+++ efeitos-no-dom/dropdown-menu/js/modules/scroll-animacao.js
@@ -0,0 +1,25 @@
+// ================ ANIMAÇÃO AO SCROLL ================
+
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+  if(sections.length) {
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;       // para animar antes de chegar ao topo
+      if(isSectionVisible) {
+        section.classList.add('ativo');
+      } else {
+        section.classList.remove('ativo');
+      }
+    })
+  }
+  animaScroll()
+
+  window.addEventListener('scroll', animaScroll)
+  }
+}
+
