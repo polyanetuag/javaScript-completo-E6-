41f5e32f1527ea43727e4b64b1f36bd669b2659c
Polyane Tuag <pollyportugues@hotmail.com>
2020-11-14 19:53:21 -0300

    estrutura_projeto

---
+++ efeitos-no-dom/date-object/js/modules/menu-mobile.js
@@ -0,0 +1,23 @@
+import outsideClick from './outsideclick.js'
+
+export default function initMenuMobile() {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click', 'touchstart']
+
+  if(menuButton) {
+    function openMenu(event) {
+      menuList.classList.add('active');
+      menuButton.classList.add('active');
+  
+      outsideClick(menuList, eventos, () => {
+        menuList.classList.remove('active')
+      })
+    }
+  
+    eventos.forEach((evento) => {
+      menuButton.addEventListener(evento, openMenu)
+    })
+  }
+}
+
