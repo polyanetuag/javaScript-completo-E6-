b2417f2c21e7ef07db0d5581522c0897c3bed6ad
Polyane Tuag <pollyportugues@hotmail.com>
2020-11-29 20:02:19 -0300

    .url e .type - Retornam o url da requisição e o tipo de resposta

--- javascript-assincrono/fetch/script.js
+++ javascript-assincrono/fetch/script.js
@@ -1,11 +1,17 @@
-// .status - Retorna o status da requisição. 
-//Se foi 404, 200, 202 e mais. ok retorna um valor booleano sendo true para uma requisição de sucesso e false para uma sem sucesso.
+// .url e .type
+//.url retorna o url da requisição. 
+//.type retorna o tipo da reposta.
 
 const cep = fetch('./docs');        // página não existe
 
 cep.then(response => {
-  console.log(response.status);
+  console.log(response.url);        // retorna a url da página 
+  console.log(response.type);        // retorna o tipo de servidor_ BASIC - requisição dentro do próprio servidor
   if(response.status === 404) {
     console.log('Essa página não existe!')
   }
 });
+
+
+
+// cors - requisição fora do servidor, porém permitida
