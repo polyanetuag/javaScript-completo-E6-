028695842e3880e613be9284bc28f46b89a0f3c9
Polyane Tuag <pollyportugues@hotmail.com>
2020-12-05 15:40:50 -0300

    Aula_Animais Fetch

--- javascript-assincrono/animais-fetch/js/script.js
+++ javascript-assincrono/animais-fetch/js/script.js
@@ -6,8 +6,8 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initTDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +17,5 @@ initModal();
 initTooltip();
 initTDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
