bd86e10ba9caf2f98bcc02d56c7f3f2fa392cf21
Polyane Tuag <pollyportugues@hotmail.com>
2020-12-16 19:41:54 -0300

    Estrutura_projeto

---
+++ automacao-front-end/animais-fetch/js/script.js
@@ -0,0 +1,21 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initTDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initTDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
